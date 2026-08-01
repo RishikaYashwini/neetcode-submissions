aa0578fa09ca8c03f83b27773283e76007529559
Rish <146313623+RishikaYashwini@users.noreply.github.com>
2026-08-01 22:24:36 +0530

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> res;
+        for(int i=0;i<=nums.size()-k;i++){
+            int maxi = nums[i];
+            for(int j=i;j<=i+k-1;j++){
+                maxi=max(maxi,nums[j]);
+            }
+            res.push_back(maxi);
+        }
+        return res;
+    }
+};
